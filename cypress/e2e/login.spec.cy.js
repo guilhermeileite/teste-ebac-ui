420bc1293880d1e9b6b08918176ed2be52b7e594
guilhermeileite <guilhermekpr@gmail.com>
2023-08-02 21:24:31 -0300

    alterações no teste automatizado

---
+++ cypress/e2e/login.spec.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="cypress" />
+
+context('Funcionalidade Login', () =>{
+
+    it('Deve fazer login com sucesso' , () => {
+
+    })
+
+    it('Deve exibir uma mensagem de erro ao inserir usuário ou senha inválidos', () =>{
+        
+    })
+})
---
+++ cypress/e2e/pre-cadastro.spec.cy.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+const { faker } = require('@faker-js/faker');
+
+describe('Funcionalidade Pré Cadastro', () => {
+
+    beforeEach(() => {
+        cy.visit('http://34.171.117.131/minha-conta/')
+    });
+
+    it('Deve completar o cadastro com sucesso', () => {
+        let nomeFaker = faker.name.firstName()
+        let sobrenomeFaker = faker.name.lastName()
+        let emailFaker = faker.internet.email(nomeFaker)
+
+        cy.get('#reg_email').type(emailFaker)
+        cy.get('#reg_password').type('!teste@teste$')
+        cy.get(':nth-child(4) > .button').click()
+
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+        cy.get('#account_first_name').type(nomeFaker)
+        cy.get('#account_last_name').type(sobrenomeFaker)
+        cy.get('.woocommerce-Button').click()
+        
+    });
+    
+});
